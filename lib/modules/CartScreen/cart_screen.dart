7c726c5caa6bc190333da05ac39daa3ac2e16020
mazenmohamed56 <50079573+mazenmohamed56@users.noreply.github.com>
2021-12-28 21:01:47 +0200

    CartScreen

--- lib/modules/CartScreen/cart_screen.dart
+++ lib/modules/CartScreen/cart_screen.dart
@@ -84,7 +84,7 @@ class CartScreen extends StatelessWidget {
                       ),
                       defaultButton(
                         function: () {
-                          CartScreenCubit.get(context).delete();
+                          CartScreenCubit.get(context).checkOut();
                         },
                         text: 'Check Out',
                         radius: 30,
@@ -103,146 +103,185 @@ class CartScreen extends StatelessWidget {
   }
 
   Widget buildCartItem(CartScreenCubit cubit, int index, BuildContext context) {
-    return Container(
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
-      child: Card(
-        elevation: 20,
-        margin: EdgeInsets.zero,
-        child: Stack(
-          alignment: AlignmentDirectional.bottomStart,
-          children: [
-            Image(
-                image: NetworkImage('${cubit.cartItems[index].imagepath}'),
-                height: 120,
-                width: double.infinity,
-                fit: BoxFit.fill),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                child: Card(
-                  elevation: 20,
-                  color: Colors.black.withOpacity(0.04),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${cubit.cartItems[index].title}',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline2!
-                                  .copyWith(color: Colors.white),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                                'Price per portion : ${cubit.cartItems[index].discount != 0 ? cubit.cartItems[index].discount : cubit.cartItems[index].price}',
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      background: Container(
+        decoration: BoxDecoration(
+            color: Colors.deepOrange[300],
+            borderRadius: BorderRadius.circular(15)),
+        width: 100,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsetsDirectional.only(start: 180),
+            child: Icon(Icons.delete),
+          ),
+        ),
+      ),
+      confirmDismiss: (direction) => showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                  title: Text('Please Confirm'),
+                  content: Text('Are you sure you want to delete?'),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop(false);
+                        },
+                        child: Text('Cancel')),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop(true);
+                        },
+                        child: Text('Confirm')),
+                  ])),
+      onDismissed: (DismissDirection direction) {
+        if (direction == DismissDirection.endToStart)
+          cubit.deletItem(index: index, id: cubit.cartItems[index].id);
+      },
+      key: Key(cubit.cartItems[index].id),
+      child: Container(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+        child: Card(
+          elevation: 20,
+          margin: EdgeInsets.zero,
+          child: Stack(
+            alignment: AlignmentDirectional.bottomStart,
+            children: [
+              Image(
+                  image: NetworkImage('${cubit.cartItems[index].imagepath}'),
+                  height: 120,
+                  width: double.infinity,
+                  fit: BoxFit.fill),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Card(
+                    elevation: 20,
+                    color: Colors.black.withOpacity(0.04),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                '${cubit.cartItems[index].title}',
                                 style: Theme.of(context)
                                     .textTheme
-                                    .bodyText2!
-                                    .copyWith(
-                                        color: Colors.white, fontSize: 16)),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                                'Total Price : ${cubit.cart[index]['totalPrice']}',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyText2!
-                                    .copyWith(
-                                        color: Colors.white, fontSize: 16)),
-                            SizedBox(
-                              height: 5,
-                            ),
-                          ],
+                                    .headline2!
+                                    .copyWith(color: Colors.white),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                  'Price per portion : ${cubit.cartItems[index].discount != 0 ? cubit.cartItems[index].discount : cubit.cartItems[index].price}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText2!
+                                      .copyWith(
+                                          color: Colors.white, fontSize: 16)),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                  'Total Price : ${cubit.cart[index]['totalPrice']}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText2!
+                                      .copyWith(
+                                          color: Colors.white, fontSize: 16)),
+                              SizedBox(
+                                height: 5,
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Material(
-                                borderRadius: BorderRadius.circular(20),
-                                elevation: 10,
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Material(
+                                  borderRadius: BorderRadius.circular(20),
+                                  elevation: 10,
+                                  child: defaultButton(
+                                      function: () {
+                                        cubit.updateCartItem(
+                                            typeOfChange: '-',
+                                            index: index,
+                                            pricePerPortion: cubit
+                                                        .cartItems[index]
+                                                        .discount !=
+                                                    0
+                                                ? cubit
+                                                    .cartItems[index].discount
+                                                : cubit.cartItems[index].price);
+                                      },
+                                      text: '-',
+                                      width: 10,
+                                      radius: 20,
+                                      height: 30),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Expanded(
                                 child: defaultButton(
-                                    function: () {
-                                      cubit.updateCartItem(
-                                          typeOfChange: '-',
-                                          index: index,
-                                          pricePerPortion: cubit
-                                                      .cartItems[index]
-                                                      .discount !=
-                                                  0
-                                              ? cubit.cartItems[index].discount
-                                              : cubit.cartItems[index].price);
-                                    },
-                                    text: '-',
-                                    width: 10,
+                                    function: () {},
+                                    text: '${cubit.cart[index]['itemCount']}',
+                                    backgroundColor: Colors.white,
+                                    fontColor: defaultColor,
+                                    borderWidth: 1,
+                                    fontSize: 13,
                                     radius: 20,
                                     height: 30),
                               ),
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            Expanded(
-                              child: defaultButton(
-                                  function: () {},
-                                  text: '${cubit.cart[index]['itemCount']}',
-                                  backgroundColor: Colors.white,
-                                  fontColor: defaultColor,
-                                  borderWidth: 1,
-                                  fontSize: 13,
-                                  radius: 20,
-                                  height: 30),
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            Expanded(
-                              child: Material(
-                                borderRadius: BorderRadius.circular(20),
-                                elevation: 10,
-                                child: defaultButton(
-                                    function: () {
-                                      cubit.updateCartItem(
-                                          typeOfChange: '+',
-                                          index: index,
-                                          pricePerPortion: cubit
-                                                      .cartItems[index]
-                                                      .discount !=
-                                                  0
-                                              ? cubit.cartItems[index].discount
-                                              : cubit.cartItems[index].price);
-                                    },
-                                    text: '+',
-                                    radius: 20,
-                                    width: 20,
-                                    height: 30),
+                              const SizedBox(
+                                width: 5,
                               ),
-                            ),
-                          ],
+                              Expanded(
+                                child: Material(
+                                  borderRadius: BorderRadius.circular(20),
+                                  elevation: 10,
+                                  child: defaultButton(
+                                      function: () {
+                                        cubit.updateCartItem(
+                                            typeOfChange: '+',
+                                            index: index,
+                                            pricePerPortion: cubit
+                                                        .cartItems[index]
+                                                        .discount !=
+                                                    0
+                                                ? cubit
+                                                    .cartItems[index].discount
+                                                : cubit.cartItems[index].price);
+                                      },
+                                      text: '+',
+                                      radius: 20,
+                                      width: 20,
+                                      height: 30),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
